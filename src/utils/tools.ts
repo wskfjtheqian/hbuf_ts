118598ee4f58c5de39f211a87beda167f59ee1d5
heqian <182359041@qq.com>
2025-11-30 15:53:23 +0800

    refactor: 优化日期格式化逻辑 style: 使用substring替代substr

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -64,23 +64,20 @@ export function formatDate(date: Date | string | number, format?: string): strin
             date = new Date(date);
             break;
     }
-    if (date instanceof Date) {
-        const dict: any = {
-            yyyy: date.getFullYear(),
-            M: date.getMonth() + 1,
-            d: date.getDate(),
-            H: date.getHours(),
-            m: date.getMinutes(),
-            s: date.getSeconds(),
-            MM: ("" + (date.getMonth() + 101)).substr(1),
-            dd: ("" + (date.getDate() + 100)).substr(1),
-            HH: ("" + (date.getHours() + 100)).substr(1),
-            mm: ("" + (date.getMinutes() + 100)).substr(1),
-            ss: ("" + (date.getSeconds() + 100)).substr(1)
-        };
-        return format.replace(/(yyyy|MM?|dd?|HH?|ss?|mm?)/g, function () {
-            return dict[arguments[0]];
-        });
-    }
-    return "" + date
+    const dict: any = {
+        yyyy: date.getFullYear(),
+        M: date.getMonth() + 1,
+        d: date.getDate(),
+        H: date.getHours(),
+        m: date.getMinutes(),
+        s: date.getSeconds(),
+        MM: ("" + (date.getMonth() + 101)).substring(1),
+        dd: ("" + (date.getDate() + 100)).substring(1),
+        HH: ("" + (date.getHours() + 100)).substring(1),
+        mm: ("" + (date.getMinutes() + 100)).substring(1),
+        ss: ("" + (date.getSeconds() + 100)).substring(1)
+    };
+    return format.replace(/(yyyy|MM?|dd?|HH?|ss?|mm?)/g, function () {
+        return dict[arguments[0]];
+    });
 }
