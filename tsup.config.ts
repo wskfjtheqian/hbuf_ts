20910eaa05e2f5391fd7f1589c44d37937f836f6
heqian <182359041@qq.com>
2026-02-02 11:25:56 +0800

    feat: 添加对浏览器环境的支持

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "tsup";
+import {defineConfig} from "tsup";
 
 export default defineConfig({
     entry: ["src/index.ts"],
-    format: ["esm", "cjs"],
+    format: ["esm", "cjs", "iife"],
     dts: {
         entry: "src/index.ts"
     },
     sourcemap: true,
     clean: true,
     target: "es2020",
+    globalName: "hbuf",
 });
